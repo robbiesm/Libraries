e8e27bea5c82e035fc28b9b55f85bfbbf55dd38b
robbiesm <r0614858@student.thomasmore.be>
2018-04-03 21:07:06 +0200

    Added powerSTEP01_Lib
    Added the basic version of powerSTEP01_Lib

---
+++ powerSTEP01_Lib/powerSTEP01_Lib/Debug/makedep.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+Library\powerSTEP01\powerSTEP01.c
+
+Library\SPI\SPI_Functions.c
+
